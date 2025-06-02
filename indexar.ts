71e9c5f07131f5b2adbc37cc7f5ca61815f9d151
PhantomOz <faniogor@gmail.com>
2025-06-02 17:36:34 +0100

    This add Indexar constructor

--- indexar.ts
+++ indexar.ts
@@ -1,4 +1,26 @@
 import { EventEmitter } from "events";
 import { ethers } from "ethers";
+import sqlite3 from "sqlite3";
 
-class Indexar extends EventEmitter {}
+class Indexar extends EventEmitter {
+  provider: ethers.JsonRpcProvider;
+  db: sqlite3.Database;
+  contracts: Map<string, ethers.Contract>;
+  lastProcessedBlock: number;
+  batchSize: number;
+  isRunning: boolean;
+
+  constructor(config: {
+    provider: ethers.JsonRpcProvider;
+    dbPath: string;
+    batchSize: number;
+  }) {
+    super();
+    this.provider = config.provider;
+    this.db = new sqlite3.Database(config.dbPath || "indexar.db");
+    this.contracts = new Map();
+    this.batchSize = config.batchSize;
+    this.lastProcessedBlock = 0;
+    this.isRunning = false;
+  }
+}
